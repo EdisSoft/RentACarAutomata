4cff5287ac0591be99d40cc70a35cf1eb0dae24f
Roland Kordován <Kordovan.Roland@konasoft.hu>
2023-04-07 21:53:26 +0200

    Revert "Delete FunctionsCore/Log directory"
    This reverts commit 35f739f1ef9f4622586633ed98ba4167733853b6.

---
+++ FunctionsCore/Log/Log.cs
@@ -0,0 +1,78 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+
+namespace FunctionsCore
+{
+    public static class Log
+    {
+        public static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+
+        public static void Trace(string message)
+        {
+            Logger.Trace(message);
+        }
+
+        public static void Trace(string message, Exception ex)
+        {
+            Logger.Trace(ex, message);
+        }
+
+        public static void Debug(string message)
+        {
+            Logger.Debug(message);
+        }
+
+        public static void Debug(string message, Exception ex)
+        {
+            Logger.Debug(ex, message);
+        }
+
+        public static void Info(string message)
+        {
+            Logger.Info(message);
+        }
+
+        public static void Info(string message, Exception ex)
+        {
+            Logger.Info(ex, message);
+        }
+
+        public static void Warning(string message)
+        {
+            Logger.Warn(message);
+        }
+
+        public static void Warning(string message, Exception ex)
+        {
+            Logger.Warn(ex, message);
+        }
+
+        public static void Error(string message)
+        {
+            Logger.Error(message);
+        }
+
+        public static void Error(string message, Exception ex)
+        {
+            Logger.Error(ex, message);
+        }
+        // for loging exception type, exception message etc..
+        public static void Error(string message, string additionalMessage)
+        {
+            Logger.Error(additionalMessage, message);
+        }
+
+        public static void Fatal(string message)
+        {
+            Logger.Fatal(message);
+        }
+
+        public static void Fatal(string message, Exception ex)
+        {
+            Logger.Fatal(ex, message);
+        }
+
+    }
+}
