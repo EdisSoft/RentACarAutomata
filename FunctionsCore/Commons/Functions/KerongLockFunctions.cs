1774adaba1b9fd57cfbc2ba737ce1338d7b8853a
Roland Kordován <Kordovan.Roland@konasoft.hu>
2023-06-26 15:27:37 +0200

    Foglalás frissítésnél állítsa vissza az előző nyelvet

--- FunctionsCore/Commons/Functions/KerongLockFunctions.cs
+++ FunctionsCore/Commons/Functions/KerongLockFunctions.cs
@@ -3,11 +3,8 @@ using FunctionsCore.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace FunctionsCore.Commons.Functions
 {
